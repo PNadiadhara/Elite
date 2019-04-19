1eac5614232e24bad837a59144d4f00365abedc4
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-04-18 23:52:24 -0400

    added the funcationailty for the users location, added the extension for the delegate and the didUpdate location function for it

--- Elite/Controllers/Map View Controllers/MapViewController.swift
+++ Elite/Controllers/Map View Controllers/MapViewController.swift
@@ -18,19 +18,37 @@ enum GoogleMapsMVState {
 class MapViewController: UIViewController {
     // MARK: - Outlets and Properties
     @IBOutlet weak var googleMapsMapView: GMSMapView!
+    
     private var googleMapsMVEditingState = GoogleMapsMVState.noMarkersShown
+    
     private var locationManager = CLLocationManager()
+    
     private var userLocation = CLLocation()
+    
     private var handballResults = [HandBall]()
+    
     private var basketballResults = [BasketBall]()
+    // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        getUsersLocations()
+    }
+    
+    private func getUsersLocations(){
         
     }
+    
     @IBAction func showBasketBallMarkers(_ sender: UIButton) {
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            googleMapsMapView.settings.compassButton = true
+            googleMapsMapView.settings.myLocationButton = true
+            googleMapsMapView.isMyLocationEnabled = true
+        }
     }
     
     @IBAction func showHandBallMarkers(_ sender: UIButton) {
@@ -41,4 +59,15 @@ class MapViewController: UIViewController {
 extension MapViewController: GMSMapViewDelegate
  {
    
+}
+extension MapViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationValue: CLLocationCoordinate2D = manager.location!.coordinate // fix the force unwrapp
+        print("lat: \(locationValue.latitude) and lng: \(locationValue.longitude) ")
+        let currentUsersLocation = locations.last
+        self.userLocation = currentUsersLocation!
+        let startPosition = GMSCameraPosition.camera(withLatitude: ( self.userLocation.coordinate.latitude), longitude: ( self.userLocation.coordinate.longitude), zoom: 14.0)
+        googleMapsMapView.animate(to: startPosition)
+        self.locationManager.stopUpdatingLocation()
+    }
 }
