77108b86286a3c766dd3ed6706b8ea017e8231ba
PN <priteshrna@gmail.com>
2019-05-03 12:46:31 -0400

    Rank Update paged updated

--- Elite/Controllers/Players View Controllers/RankChangeController.swift
+++ Elite/Controllers/Players View Controllers/RankChangeController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class RankChangeController: UIViewController {
-
+var rank = 2700
     @IBOutlet var playerImageView: CircularRedImageView!
     @IBOutlet var playerUserName: UILabel!
     @IBOutlet var currentRankScore: UILabel!
@@ -17,23 +17,44 @@ class RankChangeController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var rank = 2500
+        
         var highestRank = 3000
         playerImageView.image = UIImage(named: TabBarViewController.currentUser.displayName! + "FightingLeft")
         currentRankScore.text = String(rank)
         highestRankScore.text = String(highestRank)
+        updateRank()
         // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateRank() {
+        
+        rank += Int.random(in: 20...40)
+        currentRankScore.pushTransition(1.5)
+        currentRankScore.text = String(rank)
     }
-    */
 
+}
+
+extension UIView {
+    func pushTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            CAMediaTimingFunctionName.easeInEaseOut)
+        animation.type = CATransitionType.push
+        animation.subtype = CATransitionSubtype.fromTop
+        animation.duration = duration
+        layer.add(animation, forKey: CATransitionType.push.rawValue)
+    }
+    
+    func pullTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            CAMediaTimingFunctionName.easeInEaseOut)
+        animation.type = CATransitionType.push
+        animation.subtype = CATransitionSubtype.fromBottom
+        animation.duration = duration
+        layer.add(animation, forKey: CATransitionType.push.rawValue)
+    }
+    
 }
