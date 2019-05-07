a504e05d6a5cfc4c40b354700fc1158a0fd9b8d1
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-05-07 14:42:32 -0400

    updated the UI of the login and create account view and also added a few files for gradient stuff

--- Elite/Controllers/Account Login View Controllers/CreateAccountViewController.swift
+++ Elite/Controllers/Account Login View Controllers/CreateAccountViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    
     @IBOutlet weak var conteinerView: UIView!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -17,34 +17,52 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var createAccountTitle: UILabel!
+    @IBOutlet weak var createNewUserBttn: UIButton!
     private var authservice = AppDelegate.authservice
     
     
     var keyboardHeight = CGFloat()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setOutletsDelegates()
+        setViewControllerSettings()
+        setupGradient()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        registerKeyboardNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unregisterKeyboardNotifications()
+    }
+    
+    private func setOutletsDelegates(){
         firstNameTextField.delegate = self
         lastNameTextField.delegate = self
         usernameTextField.delegate = self
         emailTextField.delegate = self
         passwordTextField.delegate = self
         confirmPasswordTextField.delegate = self
+    }
+    
+    private func setViewControllerSettings(){
         let screenTap = UITapGestureRecognizer.init(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(screenTap)
-    authservice.authserviceCreateNewAccountDelegate = self
+        authservice.authserviceCreateNewAccountDelegate = self
     }
- 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        registerKeyboardNotification()
+    private func setupGradient(){
+        view.setGradientFromRightToLeft(colorOne: UIColor.black, colorTwo: UIColor.lightGrey)
+        createNewUserBttn.setGradientFromUpperLeftToBottmRight(colorOne: .eliteGold, colorTwo: .eliteGold2)
+        conteinerView.setGradientFromRightToLeft(colorOne: UIColor.black, colorTwo: UIColor.lightGrey)
+        createAccountTitle.textColor = .gold
     }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        unregisterKeyboardNotifications()
-    }
-    @IBAction func createButtonPressed(_ sender: UIButton) {
+    private func createNewUser(){
         guard let email = emailTextField.text,
             let password = passwordTextField.text,
             let confirmPassword = confirmPasswordTextField.text,
@@ -57,7 +75,7 @@ class CreateAccountViewController: UIViewController {
             !firstName.isEmpty,
             !lastName.isEmpty,
             !userName.isEmpty
-                else {
+            else {
                 showAlert(title: "Missing Required Fields", message: "Email and Password Required")
                 return
         }
@@ -66,16 +84,26 @@ class CreateAccountViewController: UIViewController {
         } else {
             authservice.createNewAccount(email: email, password: password, firstName: firstName, lastName: lastName, username: userName)
         }
-
+        
     }
-    @IBAction func existingUserBttnPressed(_ sender: UIButton){
+    
+    private func goBackToPreviousVC(){
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func createButtonPressed(_ sender: UIButton) {
+        createNewUser()
+    }
+    
+    @IBAction func existingUserBttnPressed(_ sender: UIButton){
+        goBackToPreviousVC()
     }
     
     private func registerKeyboardNotification(){
         NotificationCenter.default.addObserver(self, selector: #selector(willShowKeyboard(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(willHideKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
     @objc private func willShowKeyboard(notification: Notification){
         guard let info = notification.userInfo,
             let keyboardFrame = info["UIKeyboardFrameEndUserInfoKey"] as? CGRect else {
@@ -84,18 +112,20 @@ class CreateAccountViewController: UIViewController {
         }
         keyboardHeight = keyboardFrame.height
         print(keyboardHeight)
-            conteinerView.transform = CGAffineTransform(translationX: 0, y: -keyboardFrame.height + 250)
+        conteinerView.transform = CGAffineTransform(translationX: 0, y: -keyboardFrame.height + 150)
     }
+    
     @objc private func willHideKeyboard(){
         conteinerView.transform = CGAffineTransform.identity
     }
+    
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
     }
+    
     private func unregisterKeyboardNotifications(){
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
-    
     @IBAction func showLoginView(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
@@ -114,21 +144,8 @@ extension CreateAccountViewController : AuthServiceCreateNewAccountDelegate {
 }
 extension CreateAccountViewController: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        if textField.text == "\n" {
-//            textField.resignFirstResponder()
-//            return true
-//        }
         textField.resignFirstResponder()
         return true
     }
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        switch textField.tag {
-//        case 0,1,2,3:
-//        conteinerView.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight)
-//        default:
-//            return
-//        }
-//
-//    }
-
+    
 }
--- Elite/Extensions/UIView+Extensions.swift
+++ Elite/Extensions/UIView+Extensions.swift
@@ -27,4 +27,112 @@ extension UIView{
             self.isHidden = true
         })
     }
+    func setGradientFromUpperLeftToBottmRight(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromBottmRightToUpperLeft(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromBottomLeftToUpperRight(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromUpperRightToBottomLeft(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromRightToLeft(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromLeftToRight(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromTopToBottm(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setGradientFromBottmToTop(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    func setMultiColorGradient(_ coreGraphicColors: [CGColor] ){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = coreGraphicColors
+        //        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        layer.cornerRadius = 5
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
+/*
+ 1. let gradientLayer = CAGradientLayer() means creating the instance
+ 
+ 2. gradientLayer.frame = bounds means setting the frame to the bounds of whatever object you want it to be ex: Button or View "Taking up the entire space"
+ 
+ 3. gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor] means its taking in an array of colors cg = core graphics which is needed for this layer
+ 
+ 4. gradientLayer.locations = [0.0, 1.0] (OPTIONAL) means the breakpoint in the gradient where the two colors mix. By default the colors mix in the middle
+ - [0.0, 1.0] means it is going to blend in the middle
+ 
+ 5. gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0) means where the gradient starts
+ - (x: 1.0, y: 1.0) means its starting at the upper left hand corner
+ 
+ 6. gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0) means where the gradient ends
+ - (x: 0.0, y: 0.0) means its ending at the lower right hand corner
+ 
+ 7. layer.insertSublayer(gradientLayer, at: 0) means inserting the gradient in the sublayer @ 0 "The First layer"
+ */
